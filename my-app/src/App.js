b45e53a0733fb56aafe73f679aaee8f56c506b20
Sean <69405243+seanpen00@users.noreply.github.com>
2023-11-27 16:53:38 -0500

    added website content to root folder

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
 import thumbnail from "./assets/thumbnail.PNG";
+import pic1 from "./assets/pic1.PNG";
+import pic2 from "./assets/pic2.PNG";
+import pic3 from "./assets/pic3.PNG";
+import talking from "./assets/talking.gif";
+import loading from "./assets/loading.gif";
+
+function ImageWithCaption({ src, caption }) {
+  return (
+    <div>
+      <img src={src} alt={caption} className="screenshot" />
+      <p style={{ fontSize: "80%" }}>{caption}</p>
+    </div>
+  );
+}
 
 function App() {
   return (
@@ -12,35 +26,54 @@ function App() {
         <br />
         <br />
         <div style={{ maxWidth: "60%" }}>
-          <p style={{ fontSize: "80%" }}>Abstract</p>
-          <p style={{ fontSize: "80%" }}>
-          Our objective is to develop a virtual reality experience that allows users to interact through conversation with an AI in a variety of ways. This would mean giving the player the ability to speak and having those spoken words converted to text. This text would then be communicated to an AI whose response would be returned via text-to-speech. Above this basic underlying structure, the player would be able to see a human model representing the AI being communicated with.
+          <p style={{marginBottom: 0}}><b>Abstract</b></p>
+          <p style={{ fontSize: "80%", marginTop: 10 }}>
+            Our objective is to develop a virtual reality experience that allows
+            users to interact through conversation with an AI in a variety of
+            ways. The player should have the ability to speak and have their
+            speech converted to text. This text would then be communicated to an
+            AI whose response would be returned via text-to-speech. Above this
+            basic underlying structure, the player would be able to see a human
+            model representing the AI being communicated with, allowing for a
+            higher degree of immersion.
           </p>
           <br />
 
-          <p style={{ fontSize: "80%" }}>Screenshots</p>
+          <div>
+            <p style={{marginBottom: 25}}><b>Screenshots</b></p>
+            <div>
+              <img src={pic1} className="screenshot"></img>
+              <p className="caption">User POV of talking to camper at a campsite</p>
+            </div>
+            <div>
+              <img src={pic2} className="screenshot"></img>
+              <p className="caption">User POV of talking to farmer at a farm</p>
+            </div>
+            <div>
+              <img src={pic3} className="screenshot"></img>
+              <p className="caption">User POV of talking to lawyer on a bus</p>
+            </div>
+            <div>
+              <img src={talking} className="gif"></img>
+              <p className="caption">Talking animation in action</p>
+            </div>
+            <div>
+              <img src={loading} className="gif"></img>
+              <p className="caption">Loading dialogue animation in action</p>
+            </div>
+            <br />
+            <br />
+            <p><b>Video</b></p>
+            <img src={thumbnail} className="screenshot"></img>
+            <br />
+            <br />
+          </div>
 
-            <img src={thumbnail} className="screenshot" ></img>
-            <img src={thumbnail} className="screenshot" ></img>
-            <img src={thumbnail} className="screenshot" ></img>
-            <img src={thumbnail} className="screenshot" ></img>
-            <img src={thumbnail} className="screenshot" ></img>
-            <img src={thumbnail} className="screenshot" ></img>
-            <img src={thumbnail} className="screenshot" ></img>
-            <img src={thumbnail} className="screenshot" ></img>
-            <img src={thumbnail} className="screenshot" ></img>
-            <img src={thumbnail} className="screenshot" ></img>
-            <br/>
-            <br/>
-            <p style={{ fontSize: "80%" }}>Video</p>
-            <img src={thumbnail} className="screenshot" ></img>
-            <br/>
-            <br/>
-            <p style={{ fontSize: "80%" }}>Team Profile</p>
-            <img src={thumbnail} className="screenshot" ></img>
-            <img src={thumbnail} className="screenshot" ></img>
-            <img src={thumbnail} className="screenshot" ></img>
-            <img src={thumbnail} className="screenshot" ></img>
+          <p><b>Team Profile</b></p>
+          <img src={thumbnail} className="screenshot"></img>
+          <img src={thumbnail} className="screenshot"></img>
+          <img src={thumbnail} className="screenshot"></img>
+          <img src={thumbnail} className="screenshot"></img>
         </div>
       </header>
     </div>
