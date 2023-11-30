e5f048cf4390b95e8722e2ab7d35e4cefa9ecda5
seanpen00 <69405243+seanpen00@users.noreply.github.com>
2023-11-30 12:52:11 -0500

    final build

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,6 +6,10 @@ import pic2 from "./assets/pic2.PNG";
 import pic3 from "./assets/pic3.PNG";
 import talking from "./assets/talking.gif";
 import loading from "./assets/loading.gif";
+import ben from "./assets/ben.png";
+import alex from "./assets/alex.png";
+import sean from "./assets/sean.png";
+import logan from "./assets/logan.png";
 
 function ImageWithCaption({ src, caption }) {
   return (
@@ -26,7 +30,7 @@ function App() {
         <br />
         <br />
         <div style={{ maxWidth: "60%" }}>
-          <p style={{marginBottom: 0}}><b>Abstract</b></p>
+          <p style={{ marginBottom: 0 }}><b>Abstract</b></p>
           <p style={{ fontSize: "80%", marginTop: 10 }}>
             Our objective is to develop a virtual reality experience that allows
             users to interact through conversation with an AI in a variety of
@@ -40,7 +44,7 @@ function App() {
           <br />
 
           <div>
-            <p style={{marginBottom: 25}}><b>Screenshots</b></p>
+            <p style={{ marginBottom: 25 }}><b>Screenshots</b></p>
             <div>
               <img src={pic1} className="screenshot"></img>
               <p className="caption">User POV of talking to camper at a campsite</p>
@@ -64,16 +68,42 @@ function App() {
             <br />
             <br />
             <p><b>Video</b></p>
-            <img src={thumbnail} className="screenshot"></img>
-            <br />
-            <br />
+            <iframe style={{width: "100%", height:"500px"}} src='https://www.youtube.com/embed/gCbuk9eNCZo'
+              frameborder='0'
+              allow='autoplay; encrypted-media'
+              allowfullscreen
+              title='video'
+            />
           </div>
+          <br />
 
           <p><b>Team Profile</b></p>
-          <img src={thumbnail} className="screenshot"></img>
-          <img src={thumbnail} className="screenshot"></img>
-          <img src={thumbnail} className="screenshot"></img>
-          <img src={thumbnail} className="screenshot"></img>
+          <div style={{ display: "flex" }}>
+            <div>
+              <img src={ben} className="pfp"></img>
+              <p className="caption" style={{ margin: "auto" }}><b>Ben Spivy</b></p>
+              <p className="caption">I'm currently a senior and computer science major at GMU graduating in May 2024. My role in Team One was both as the team leader and as a developer. I worked on the backend and frontend working to connect the STT and AI Chat as well as integrating the main backend into the VR environment.
+              </p>
+            </div>
+            <div>
+              <img src={alex} className="pfp"></img>
+              <p className="caption" style={{ margin: "auto" }}><b>Alex Rose</b></p>
+              <p className="caption">I am currently a senior majoring in Computer Science at George Mason University.  I will be graduating May 2024 with a BS CS.  My main contributions to the project were the VR implementation, controller handling/modelling, and making the tts/stt work in VR.
+              </p>
+            </div>
+            <div>
+              <img src={logan} className="pfp"></img>
+              <p className="caption" style={{ margin: "auto" }}><b>Logan Goldfedder</b></p>
+              <p className="caption">I am currently a senior majoring in Computer Science at George Mason University.  I will be graduating May 2024 with a Bachelors of Science in Computer Science.  My main contributions to the project were in testing, designing, requirement completion, and document creation.
+              </p>
+            </div>
+            <div>
+              <img src={sean} className="pfp"></img>
+              <p className="caption" style={{ margin: "auto" }}><b>Sean Pentiboyina</b></p>
+              <p className="caption">I'm a senior computer science major at George Mason University. I will be graduating December 2023 with a B.S. in Computer Science. My contributions to this project include implementing text-to-speech, adding NPC animations, and creating this website.</p>
+            </div>
+          </div>
+          <br />
         </div>
       </header>
     </div>
